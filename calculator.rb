6ee113e2c74ef65534edd4755049850ef8ec201f
Clarion Heard <clahon54@gmail.com>
2019-12-03 17:06:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,8 @@
 first_number = get_variable_from_file('./calculator.rb', "first_number")
 
 second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
